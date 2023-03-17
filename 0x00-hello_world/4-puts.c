d51402a64269fa8ec06ea44e109a8cd79bc451f6
Khadija1000 <daoudikhadijaa@gmail.com>
2023-03-16 18:59:01 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,14 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle"
+ *
  * Return: Always 0.
  */
+
 int main(void)
+
 {
-puts ("\"Programming is like building a multilingual puzzle"); 
-return(0);
+    puts("\"Programming is like building a multilingual puzzle"); 
+    
+    return (0);
 }
