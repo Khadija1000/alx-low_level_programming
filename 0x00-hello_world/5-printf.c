c8bab710f5a2eca14309c3363f4d36fb8b4bd13d
Khadija1000 <daoudikhadijaa@gmail.com>
2023-03-16 19:23:07 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is 
- * a piece of art," followed by a new line.
+ * a piece of art," followed newline
  * Return: Always 0.
  *
  */
+
 
 int main(void)
 
