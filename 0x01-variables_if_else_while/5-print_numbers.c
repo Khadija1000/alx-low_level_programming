1494a2574c9004e7708af127f2aa514ae8df8a16
Khadija1000 <daoudikhadijaa@gmail.com>
2023-03-17 11:21:34 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all single digit numbers
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+		printf("\n");
+	return (0);
+}
