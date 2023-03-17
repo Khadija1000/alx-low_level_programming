589fae4e5b66419e7c23a3675272ee30645d7930
Khadija1000 <daoudikhadijaa@gmail.com>
2023-03-17 13:51:26 -0700

    Test6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all single digit numbers
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d < 10; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
