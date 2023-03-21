89572963d01aec029f14471d82bf6901e17e90fe
Khadija1000 <daoudikhadijaa@gmail.com>
2023-03-21 05:21:50 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints putchar
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putcha,\n");
+	return (0);
+}
