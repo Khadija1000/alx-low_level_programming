fdcf80ea3b91d6e91fd3a2d1738594a7b31ddbfd
Khadija1000 <daoudikhadijaa@gmail.com>
2023-03-21 19:29:21 -0700

    Task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,14 +14,14 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
-	printf("%d", j);
+	printf("%ld", j);
 	els if (i == 1)
-	printf(", %d", k);
+	printf(", %ld", k);
 	else
 	{
 	k += j;
 	j = k - j;
-	printf(", %d", k);
+	printf(", %ld", k);
 	}
 	i++;
 	}
