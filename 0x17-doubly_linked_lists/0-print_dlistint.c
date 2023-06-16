d5bf1ee5775eced39d96f813aff7958555814671
Khadija1000 <daoudikhadijaa@gmail.com>
2023-06-16 13:51:06 -0700

    task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,10 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h)
 	{
-		if (!h->n)
-			printf("[0] (nil)\n");
-		else
-			printf("%d\n",  h->n);
+		printf("%d\n",  h->n);
 		h = h->next;
 		s++;
 	}
