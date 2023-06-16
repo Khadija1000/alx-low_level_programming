bd8a669850a5522d3d782a4f8ddf5ea11045ad0a
Khadija1000 <daoudikhadijaa@gmail.com>
2023-06-16 13:30:41 -0700

    task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,8 @@
 #include "lists.h"
 
 /**
- * print_dlistint - a function that prints all the elements of a dlistint_t list.
+ * print_dlistint - a function that prints all the elements
+ * of a dlistint_t list.
  * @h: pointer to the dlistint_t list to print
  *
  * Return: the number of nodes printed
